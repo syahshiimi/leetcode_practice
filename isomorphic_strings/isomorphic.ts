b7a1f0eba21d56650049e2d134aafb04230d65c1
Syahrul Anuar <syahrlanuar95@gmail.com>
2023-03-07 02:21:17 +0800

    use conditional loops to check if prev values are thea same

--- isomorphic_strings/isomorphic.ts
+++ isomorphic_strings/isomorphic.ts
@@ -23,18 +23,45 @@ function getCreateHashmap(s, t) {
 
   console.log("hashes are mapped...");
 
-  for (let e = 0; e <= s.length - 1; e++) {
-    let c1 = s_hashmap.get(e);
-    let c2 = t_hashmap.get(e);
+  //   for (let e = 0; e <= s.length - 1; e++) {
+  //     let c1 = s_hashmap.get(e);
+  //     let c2 = t_hashmap.get(e);
 
-    // console.log(c1, c2);
+  // console.log(c1, c2);
 
-    // g => n cannot be mapped if
-    // g exists in the same c1 map
-    // and already not mapped to the same value of where it is meant to map
-    //
-    if (c1 != t_hashmap.get(e)) {
-      console.log(c1, t_hashmap.get(e));
+  // egg to act
+  // c1 / c2
+  // e => a can be mapped
+  // g => c can be mapped
+  // g => n cannot be mapped because
+  // another g exists in the same c1 map
+  // and its adjacent c2 value is not the same
+  // as the previous c2 value
+  //
+
+  const s1entry = s_hashmap.entries();
+  const t1entry = t_hashmap.entries();
+
+  const s1value = s_hashmap.values();
+  const t1value = t_hashmap.values();
+
+  // for (let e = 0; e <= s.length - 1; e++) {
+  //   const s1entryChar = s1entry.next().value;
+  //   const t1entryChar = t1entry.next().value;
+  //   console.log(s1entryChar[1]);
+  //   console.log(t1entryChar[1]);
+  // }
+
+  for (let s_entry of s_hashmap.entries()) {
+    const c1nextITer = s1entry.next().value;
+    const c2nextITer = t1entry.next().value;
+    // console.log(c1nextITer[1], c2nextITer[1], s_entry[1], c2nextITer[1]);
+    console.log(t1value.next().value);
+
+    if (c1nextITer[1] === s_entry[1] && c2nextITer[1] != t1value.next().value) {
+      return false;
+    } else {
+      return console.log(true);
     }
   }
 }
